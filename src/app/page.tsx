3e610364d7c10fbec3fe7e6d99ef3815c9501c30
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2025-08-18 20:51:28 -0400

    Update copy and mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
             Turn items you don&apos;t use into instant buying power. <strong>It&apos;s like getting everything for free.</strong>
           </p>
           <p className="text-md text-gray-500 text-center lg:text-left max-w-md">
-          When you make a purchase with the Aerium card, we instantly pay you out at the market price sourced across multiple platforms. We take those items, sell them on your behalf. If they don&apos;t sell that&apos;s on us. To cover our costs, we take a flat 5% fee on all transactions.
+          When you make a purchase with the Aerium card, we instantly pay you out at the market price sourced across multiple platforms. We take those items and sell them on your behalf. If they don&apos;t sell that&apos;s on us. To cover our costs, we take a flat 5% fee on all transactions.
           </p>
           <p className="text-md text-gray-500 text-center lg:text-left max-w-md italic">
             More shares = more chances to be one of the first people to gain access!
@@ -228,7 +228,7 @@ export default function Home() {
           <Image 
             src="/iphone_mock.png" 
             alt="iPhone App Mockup" 
-            width={400 * 1.2} 
+            width={400 * 1.2}
             height={800 * 1.2} 
             className="max-w-full h-auto"
           />
