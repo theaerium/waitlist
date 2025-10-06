db89dab2f00e55beab3ff9f955b78c0d1d289410
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2025-10-06 10:37:12 -0400

    Add link to substack blog

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,23 +1,29 @@
-
-'use client';
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
-import theme from '../config/theme';
+import theme from "../config/theme";
 
 export default function Footer() {
   return (
-    <footer className="text-white" style={{ backgroundColor: theme.colors.orange }}>
+    <footer
+      className="text-white"
+      style={{ backgroundColor: theme.colors.orange }}
+    >
       <div className="px-4 sm:px-6 lg:px-8 py-16">
         <div className="w-full">
-          
           {/* Mobile Layout */}
           <div className="block md:hidden space-y-8">
             {/* Need Help Section */}
             <div className="text-left">
-              <p className="text-sm mb-2">Available daily, 8am to 10:00 PM ET</p>
+              <p className="text-sm mb-2">
+                Available daily, 8am to 10:00 PM ET
+              </p>
               <h3 className="text-xl font-medium mb-2">Need Help?</h3>
-              <a href="tel:+12899716341" className="text-2xl font-semibold text-black">
+              <a
+                href="tel:+12899716341"
+                className="text-2xl font-semibold text-black"
+              >
                 1 (289) 971-6341
               </a>
             </div>
@@ -30,27 +36,42 @@ export default function Footer() {
                 </h3>
                 <ul className="space-y-3">
                   <li>
-                    <Link href="/terms-of-service" className="text-white hover:text-gray-200 text-sm transition-colors">
+                    <Link
+                      href="/terms-of-service"
+                      className="text-white hover:text-gray-200 text-sm transition-colors"
+                    >
                       Terms of Service
                     </Link>
                   </li>
                   <li>
-                    <Link href="/condition-eligibility" className="text-white hover:text-gray-200 text-sm transition-colors">
+                    <Link
+                      href="/condition-eligibility"
+                      className="text-white hover:text-gray-200 text-sm transition-colors"
+                    >
                       Condition & Eligibility Guidelines
                     </Link>
                   </li>
                   <li>
-                    <Link href="/photo-guidelines" className="text-white hover:text-gray-200 text-sm transition-colors">
+                    <Link
+                      href="/photo-guidelines"
+                      className="text-white hover:text-gray-200 text-sm transition-colors"
+                    >
                       Photo Guidelines
                     </Link>
                   </li>
                   <li>
-                    <Link href="/privacy-policy" className="text-white hover:text-gray-200 text-sm transition-colors">
+                    <Link
+                      href="/privacy-policy"
+                      className="text-white hover:text-gray-200 text-sm transition-colors"
+                    >
                       Privacy Policy
                     </Link>
                   </li>
                   <li>
-                    <Link href="/anti-money-laundering" className="text-white hover:text-gray-200 text-sm transition-colors">
+                    <Link
+                      href="/anti-money-laundering"
+                      className="text-white hover:text-gray-200 text-sm transition-colors"
+                    >
                       Anti-Money Laundering Policy
                     </Link>
                   </li>
@@ -60,16 +81,43 @@ export default function Footer() {
               <div>
                 <h3 className="text-lg font-bold mb-4">Stay in touch</h3>
                 <div className="flex space-x-4 mb-6">
-                  <a href="https://www.instagram.com/joinaerium" className="text-white hover:text-gray-200 transition-colors">
+                  <a
+                    href="https://www.instagram.com/joinaerium"
+                    className="text-white hover:text-gray-200 transition-colors"
+                  >
                     <span className="sr-only">Instagram</span>
-                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                    <svg
+                      className="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                     </svg>
                   </a>
-                  <a href="https://x.com/joinaerium" className="text-white hover:text-gray-200 transition-colors">
+                  <a
+                    href="https://x.com/joinaerium"
+                    className="text-white hover:text-gray-200 transition-colors"
+                  >
                     <span className="sr-only">X</span>
-                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                    <svg
+                      className="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
                       <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                    </svg>
+                  </a>
+                  <a
+                    href="https://joinaerium.substack.com/"
+                    className="text-white hover:text-gray-200 transition-colors"
+                  >
+                    <span className="sr-only">X</span>
+                    <svg
+                      className="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 18.11 22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z" />
                     </svg>
                   </a>
                   {/* <a href="https://www.linkedin.com/company/aerium" className="text-white hover:text-gray-200 transition-colors">
@@ -79,11 +127,12 @@ export default function Footer() {
                     </svg>
                   </a> */}
                 </div>
-                
+
                 <div>
                   <h3 className="text-lg font-bold mb-2">Chat with us</h3>
                   <p className="text-white">
-                    Available 24/7 in the<br />
+                    Available 24/7 in the
+                    <br />
                     mobile app
                   </p>
                 </div>
@@ -92,13 +141,15 @@ export default function Footer() {
 
             {/* Disclaimer */}
             <div className="text-left text-sm">
-              <p>*Aerium is a financial services product, not a bank. Prepaid debit cards are issued by People&apos;s Trust via Reloadly</p>
+              <p>
+                *Aerium is a financial services product, not a bank. Prepaid
+                debit cards are issued by People&apos;s Trust via Reloadly
+              </p>
             </div>
           </div>
 
           {/* Desktop Layout */}
           <div className="hidden md:grid grid-cols-5 gap-8 items-start">
-            
             {/* Legal section */}
             <div className="col-span-1">
               <h3 className="text-sm font-semibold text-white tracking-wider uppercase mb-6">
@@ -106,27 +157,42 @@ export default function Footer() {
               </h3>
               <ul className="space-y-4">
                 <li>
-                  <Link href="/terms-of-service" className="text-white hover:text-gray-900 text-sm transition-colors">
+                  <Link
+                    href="/terms-of-service"
+                    className="text-white hover:text-gray-900 text-sm transition-colors"
+                  >
                     Terms of Service
                   </Link>
                 </li>
                 <li>
-                  <Link href="/condition-eligibility" className="text-white hover:text-gray-900 text-sm transition-colors">
+                  <Link
+                    href="/condition-eligibility"
+                    className="text-white hover:text-gray-900 text-sm transition-colors"
+                  >
                     Condition & Eligibility Guidelines
                   </Link>
                 </li>
                 <li>
-                  <Link href="/photo-guidelines" className="text-white hover:text-gray-900 text-sm transition-colors">
+                  <Link
+                    href="/photo-guidelines"
+                    className="text-white hover:text-gray-900 text-sm transition-colors"
+                  >
                     Photo Guidelines
                   </Link>
                 </li>
                 <li>
-                  <Link href="/privacy-policy" className="text-white hover:text-gray-900 text-sm transition-colors">
+                  <Link
+                    href="/privacy-policy"
+                    className="text-white hover:text-gray-900 text-sm transition-colors"
+                  >
                     Privacy Policy
                   </Link>
                 </li>
                 <li>
-                  <Link href="/anti-money-laundering" className="text-white hover:text-gray-900 text-sm transition-colors">
+                  <Link
+                    href="/anti-money-laundering"
+                    className="text-white hover:text-gray-900 text-sm transition-colors"
+                  >
                     Anti-Money Laundering Policy
                   </Link>
                 </li>
@@ -136,16 +202,43 @@ export default function Footer() {
             <div className="col-span-1">
               <h3 className="text-lg font-bold mb-6">Stay in touch</h3>
               <div className="flex space-x-4 mb-8">
-                <a href="https://www.instagram.com/joinaerium" className="text-white hover:text-gray-200 transition-colors">
+                <a
+                  href="https://www.instagram.com/joinaerium"
+                  className="text-white hover:text-gray-200 transition-colors"
+                >
                   <span className="sr-only">Instagram</span>
-                  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                  <svg
+                    className="h-6 w-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                   </svg>
                 </a>
-                <a href="https://x.com/joinaerium" className="text-white hover:text-gray-200 transition-colors">
+                <a
+                  href="https://x.com/joinaerium"
+                  className="text-white hover:text-gray-200 transition-colors"
+                >
                   <span className="sr-only">X</span>
-                  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <svg
+                    className="h-6 w-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
                     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                  </svg>
+                </a>
+                <a
+                  href="https://joinaerium.substack.com/"
+                  className="text-white hover:text-gray-200 transition-colors"
+                >
+                  <span className="sr-only">X</span>
+                  <svg
+                    className="h-6 w-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 18.11 22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z" />
                   </svg>
                 </a>
                 {/* <a href="https://www.linkedin.com/company/aerium" className="text-white hover:text-gray-200 transition-colors">
@@ -155,11 +248,12 @@ export default function Footer() {
                   </svg>
                 </a> */}
               </div>
-              
+
               <div>
                 <h3 className="text-lg font-bold mb-4">Chat with us</h3>
                 <p className="text-white">
-                  Available 24/7 in the<br />
+                  Available 24/7 in the
+                  <br />
                   mobile app
                 </p>
               </div>
@@ -167,10 +261,10 @@ export default function Footer() {
 
             <div className="col-span-3 flex justify-center md:justify-end">
               <div className="">
-                <Image 
-                  src="/distribution/qr_code.png" 
+                <Image
+                  src="/distribution/qr_code.png"
                   alt="QR Code"
-                  width={400} 
+                  width={400}
                   height={400}
                   className="w-fit h-fit"
                   draggable={false}
@@ -181,7 +275,10 @@ export default function Footer() {
 
           <div className="hidden md:block mt-8">
             <div className="text-left text-sm">
-              <p>*Aerium is a financial services product, not a bank. Prepaid debit cards are issued by People&apos;s Trust via Reloadly</p>
+              <p>
+                *Aerium is a financial services product, not a bank. Prepaid
+                debit cards are issued by People&apos;s Trust via Reloadly
+              </p>
             </div>
           </div>
         </div>
